22cea5c5d314fd634fe5f360dd14f116cdba6ae1
jinsnowy <yhjin210@gmail.com>
2022-12-18 22:20:14 +0900

    others

--- Tester/pch.h
+++ Tester/pch.h
@@ -4,5 +4,30 @@
 
 #pragma once
 
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+#include <algorithm>
+#include <bitset>
+#include <mutex>
+#include <memory>
+#include <functional>
+#include <thread>
+#include <future>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <type_traits>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+
