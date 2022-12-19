96bafc64017ec99981cd07e6cfd889cd574dfe9e
yhjin210 <yhjin210@adenasoft.com>
2022-12-19 16:05:28 +0900

    AuditManager 단위 테스트 코드 추가

--- Tester/06_단위_테스트_스타일.cpp
+++ Tester/06_단위_테스트_스타일.cpp
@@ -4,6 +4,45 @@
 #include "06_단위_테스트_스타일\FileSystem.h"
 #include "MockFileSystem.h"
 
+TEST(TestAuditManager, 기존_파일_없을때_AUDIT_1_TXT_파일에_레코드_생성) {
+	// given
+	MockFileSystem fileSystem;
+	AuditManager sut(fileSystem, 3, ".");
+
+	ON_CALL(fileSystem, GetFiles("."))
+		.WillByDefault(Return(vector<string>{}));
+
+	EXPECT_CALL(fileSystem, WriteLine("./audit_1.txt", "Alice;2019-04-06T18:00:00"))
+		.Times(1);
+
+	// when
+	sut.AddRecord("Alice", "2019-04-06T18:00:00");
+}
+
+TEST(TestAuditManager, 기존_파일_충분할때_기존_파일에_레코드_생성) {
+	// given
+	MockFileSystem fileSystem;
+	AuditManager sut(fileSystem, 3, ".");
+
+	ON_CALL(fileSystem, GetFiles("."))
+		.WillByDefault(Return(vector<string>{
+		"./audit_1.txt"
+	}));
+
+	ON_CALL(fileSystem, ReadLines("./audit_1.txt"))
+		.WillByDefault(Return(vector<string>{
+		"Peter;2019-04-06T16:30:30",
+		"Jane;2019-11-11T09:30:00",
+	}));
+
+
+	EXPECT_CALL(fileSystem, WriteLine("./audit_1.txt", "Alice;2019-04-06T18:00:00"))
+		.Times(1);
+
+	// when
+	sut.AddRecord("Alice", "2019-04-06T18:00:00");
+}
+
 TEST(TestAuditManager, 기존_파일_FULL일때_새로운_파일에_레코드_생성) {
 	// given
 	MockFileSystem fileSystem;
